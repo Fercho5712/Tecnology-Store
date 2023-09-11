6efceea790f65e9d573840106d44b541b75def77
Jramoz08 <yesusrimos@gmail.com>
2023-09-11 16:05:35 -0500

    Proyecto-Ionic

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  ApiKeyGoogleMaps: 'AIzaSyC-----api key',
+  firebaseConfig: {
+    apiKey: 'AIzaS-----',
+    authDomain: "gasdomi.firebaseapp.com",
+    databaseURL: "https://gas-----rebaseio.com",
+    projectId: "ga-----omi",
+    storageBucket: "gasdo-----pot.com",
+    messagingSenderId: "67-----046",
+    appId: "1:67436-----50637",
+    measurementId: "G-ME-----V4Z19"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  ApiKeyGoogleMaps: 'AIzaSyC-----api key',
+  firebaseConfig: {
+    apiKey: 'AIzaS-----',
+    authDomain: "gasdomi.firebaseapp.com",
+    databaseURL: "https://gas-----rebaseio.com",
+    projectId: "ga-----omi",
+    storageBucket: "gasdo-----pot.com",
+    messagingSenderId: "67-----046",
+    appId: "1:67436-----50637",
+    measurementId: "G-ME-----V4Z19"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
